44733a1c6d986c29aa3dd0e5440ed8a291269ce3
funkydude <funkydude@users.noreply.github.com>
2009-08-14 21:55:26 +0000

    v2.0.0 addon completely redesigned to use the friends list instead of performing who lookups, should fix several problems including the fact that Who has a cooldown, and not getting whispers from characters with small names like "Ha".

--- BadBoy_Levels.lua
+++ BadBoy_Levels.lua
@@ -1,7 +1,9 @@
 
 --good players(guildies/friends), maybe(for processing)
-local good, maybe, count, badboy, t, wholib = {}, {}, {}, CreateFrame("Frame", "BadBoy_Levels"), 1, nil
+local good, maybe, badboy, filterName = {}, {}, CreateFrame("Frame", "BadBoy_Levels"), nil
 local whisp = "You need to be level %d to whisper me."
+local err_one = "You should have a maximum of 48 friends for this addon to work properly."
+local err_two = "You have more than 48, remove %d friends."
 
 do
 	local L = GetLocale()
@@ -16,28 +18,40 @@ do
 	end
 end
 
+ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", function(_,_,msg)
+	if not filterName then return end
+	if msg == (ERR_FRIEND_ADDED_S):format(filterName) or msg == (ERR_FRIEND_REMOVED_S):format(filterName) then
+		return true
+	end
+end)
+
 badboy:Hide() --hide, don't run the onupdate
-badboy:RegisterEvent("WHO_LIST_UPDATE")
 badboy:RegisterEvent("PLAYER_LOGIN")
 badboy:RegisterEvent("FRIENDLIST_UPDATE")
 badboy:RegisterEvent("GUILD_ROSTER_UPDATE")
 badboy:SetScript("OnEvent", function(_, evt, update)
-	if evt == "WHO_LIST_UPDATE" then
-		if not wholib then --Only if WhoLib isn't running
-			badboy:Hide() --stop the onupdate
-			t = 1 --reset counter
-			FriendsFrame:RegisterEvent("WHO_LIST_UPDATE") --restore friends frame
-			SetWhoToUI(0) --restore friends frame
+	if evt == "PLAYER_LOGIN" then
+		--update our safe list on login with guild/friends
+		if IsInGuild() then
+			GuildRoster()
 		end
-		--we get all who results to prevent any strange situation where the player we want might be 2nd in
-		--the list, if we only scanned first in the list, we would create an infinite loop
-		local num = GetNumWhoResults()
-		local found = nil
+		ShowFriends()
+		local num = GetNumFriends()
+		if num and num > 48 then
+			print("|cFF33FF99BadBoy_Levels|r: "..err_one)
+			print("|cFF33FF99BadBoy_Levels|r: "..err_two:format(num-20))
+		end
+		good[UnitName("player")] = true --add ourself
+	elseif evt == "FRIENDLIST_UPDATE" then
+		local num = GetNumFriends()
 		for i = 1, num do
-			local player, _, level = GetWhoInfo(i)
+			local player, level = GetFriendInfo(i)
+			if not player then
+				ShowFriends()
+				return
+			end
 			if maybe[player] then --do we need to process this person?
-				count[player] = nil --remove counter entry
-				found = true --we found someone in this who
+				RemoveFriend(i) --Remove player from friends list
 				if level <= (tonumber(BADBOY_LEVEL) or 1) then
 					--lower than or equal to level 1, or a level defined by the user = bad
 					--so whisper the bad player what level they must be to whisper us
@@ -64,50 +78,11 @@ badboy:SetScript("OnEvent", function(_, evt, update)
 				end
 				wipe(maybe[player]) --remove player data table
 				maybe[player] = nil --remove remaining empty table
-			end
-		end
-		if not found then -- we didn't find anyone in this who, should really never happen
-			for k,v in pairs(count) do --cycle through players in counter
-				count[k] = v + 1 --add 1 to every player
-				--if player has been who'd 10 times (1 minute, reasonable relog time), give up and remove the whisper,
-				--it was probably a quick log on/off gold spammer
-				if v > 9 then
-					for _, n in pairs(maybe[k]) do
-						for _, p in pairs(n) do
-							wipe(p) --remove player data table
-						end
-						wipe(n) --remove player data table
-					end
-					wipe(maybe[k]) --remove player data table
-					maybe[k] = nil --remove remaining empty table
-					count[k] = nil --remove player counter
-				end
-			end
-		end
-		--turn on the onupdate if we still have players for processing
-		for k in pairs(maybe) do
-			if wholib then
-				wholib:Who(k) --We have to use wholib if it's installed, it doesn't like others using who
 			else
-				badboy:Show() --start the onupdate data request
+				--if they are not in the maybe list, the friend already existed or was added manually
+				--to the friends list, so we should add them to the safe list too
+				good[player] = true
 			end
-			return
-		end
-	elseif evt == "PLAYER_LOGIN" then
-		--update our safe list on login with guild/friends
-		if IsInGuild() then
-			GuildRoster()
-		end
-		ShowFriends()
-		good[UnitName("player")] = true --add ourself
-		wholib = _G.LibStub and _G.LibStub:GetLibrary("LibWho-2.0", true) or nil --I really dislike this lib
-	elseif evt == "FRIENDLIST_UPDATE" then
-		--get all online and offline friends
-		local num = GetNumFriends()
-		for i = 1, num do
-			local n = GetFriendInfo(i)
-			--add friend to good list
-			if n then good[n] = true end
 		end
 	else
 		--back down if not in a guild
@@ -125,19 +100,6 @@ badboy:SetScript("OnEvent", function(_, evt, update)
 	end
 end)
 
-badboy:SetScript("OnUpdate", function(_, e)
-	t = t + e
-	if t > 1 then --throttle, request data once a second until we get it, it might be on cooldown
-		t = 0 --reset counter
-		FriendsFrame:UnregisterEvent("WHO_LIST_UPDATE") --don't show the who popup
-		SetWhoToUI(1) --don't show results in chat
-		for k in pairs(maybe) do
-			SendWho(k) --sendwho any players needing processing
-			return
-		end
-	end
-end)
-
 --main whisper filtering cuntion
 ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", function(...)
 	--don't filter if good or GM
@@ -145,7 +107,8 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", function(...)
 	if good[player] then return end
 	local flag = select(8, ...)
 	if flag == "GM" then return end
- 
+
+	filterName = player
 	--not good or GM, added to maybe
 	if not maybe[player] then maybe[player] = {} end
 	local f = tostring(...)
@@ -154,16 +117,11 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", function(...)
 	--one table per id, incase we got more than one whisper from a player whilst still processing
 	local id = select(13, ...)
 	maybe[player][f][id] = {}
-	for i = 1, 14 do
+	for i = 1, select("#", ...) do
 		--store all the chat arguments incase we need to add it back (if it's a new good guy)
 		maybe[player][f][id][i] = select(i, ...)
 	end
-	if wholib then
-		wholib:Who(player) --We have to use wholib if it's installed, it doesn't like others using who
-	else
-		badboy:Show() --start the onupdate data request
-	end
-	count[player] = 0 --add to who counter entry
+	AddFriend(player)
 	return true --filter everything not good (maybe) and not GM
 end)
 
