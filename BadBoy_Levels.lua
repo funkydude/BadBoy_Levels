bb4cdfb1a7661fa5c22aa25821f08ca0be084a39
funkydude <funkydude@users.noreply.github.com>
2017-04-26 04:53:01 +0100

    Settings have been reset. Translations required. Added separate level boxes for DK and DH. Added option to block all whispers, with sub-options to allow from friends/guild/group. Blocking all whispers will send a different auto-reply: "You do not meet the requirements to whisper me."

--- BadBoy_Levels.lua
+++ BadBoy_Levels.lua
@@ -2,38 +2,48 @@
 --good players(guildies/friends), maybe(for processing)
 local badboy = CreateFrame("Frame")
 local good, maybe, filterTable = {}, {}, {}
-local login = nil
+local login = false
 local whisp = "BadBoy_Levels: You need to be level %d to whisper me."
+local whisp_notallowed = "BadBoy_Levels: You do not meet the requirements to whisper me."
 local err = "You have reached the maximum amount of friends, remove 2 for this addon to function properly!"
 
 do
 	local L = GetLocale()
 	if L == "esES" or L == "esMX" then
 		whisp = "BadBoy_Levels: Necesitas ser nivel %d para susurrarme."
+		--whisp_notallowed = "BadBoy_Levels: You do not meet the requirements to whisper me."
 		err = "Has llegado a la cantidad máxima de amigos, quita 2 amigos para que este addon funcione propiamente."
 	elseif L == "ptBR" then
 		whisp = "BadBoy_Levels: Você precisa ter nível %d para me sussurrar."
+		--whisp_notallowed = "BadBoy_Levels: You do not meet the requirements to whisper me."
 		err = "Você atingiu o numero máximo de amigos, remova 2 para este addon funcionar corretamente!"
 	elseif L == "deDE" then
 		whisp = "BadBoy_Levels: Du musst Level %d sein, um mir etwas flüstern zu können."
+		--whisp_notallowed = "BadBoy_Levels: You do not meet the requirements to whisper me."
 		err = "Du hast die maximale Anzahl an Freunden erreicht, bitte entferne 2, damit dieses Addon richtig funktioniert!"
 	elseif L == "frFR" then
 		whisp = "BadBoy_Levels: Vous devez être au moins de niveau %d pour me chuchoter."
+		--whisp_notallowed = "BadBoy_Levels: You do not meet the requirements to whisper me."
 		err = "Vous avez atteint la limite de contenu de votre liste d'amis. Enlevez-en 2 pour que cet addon fonctionne correctement !"
 	elseif L == "ruRU" then
 		whisp = "BadBoy_Levels: Вы должны быть уровнем не ниже %d, что бы шептать мне."
+		--whisp_notallowed = "BadBoy_Levels: You do not meet the requirements to whisper me."
 		err = "Вы достигли максимального количества друзей, удалите двоих для нормальной работы модификации!"
 	elseif L == "koKR" then
 		whisp = "BadBoy_Levels: 저에게 귓속말을 보내기 위해서는 레벨 %d이 필요합니다."
+		--whisp_notallowed = "BadBoy_Levels: You do not meet the requirements to whisper me."
 		err = "친구 목록이 최대한도에 도달했습니다. 제대로 애드온이 작업을 하기 위해서는 2명을 제거해야 합니다!"
 	elseif L == "zhTW" then
 		whisp = "BadBoy_Levels: 你起碼要達到 %d 級才能密我。"
+		--whisp_notallowed = "BadBoy_Levels: You do not meet the requirements to whisper me."
 		err = "你的好友列表滿了，此插件需要你騰出2個好友空位!"
 	elseif L == "zhCN" then
 		whisp = "BadBoy_Levels: 你起码要达到 %d 级才能和我讲话。"
+		--whisp_notallowed = "BadBoy_Levels: You do not meet the requirements to whisper me."
 		err = "你的好友列表已满，此模块需要你腾出2个好友空位！"
 	elseif L == "itIT" then
 		whisp = "BadBoy_Levels: E' necessario che tu sia di livello %d per sussurrarmi."
+		--whisp_notallowed = "BadBoy_Levels: You do not meet the requirements to whisper me."
 		err = "Hai raggiunto il limite massimo di amici, rimuovine 2 per permettere a questo addon di funzionare correttamente!"
 	end
 end
@@ -60,88 +70,124 @@ do
 end
 
 badboy:RegisterEvent("PLAYER_LOGIN")
-badboy:RegisterEvent("FRIENDLIST_UPDATE")
-badboy:RegisterEvent("CHAT_MSG_SYSTEM")
-badboy:SetScript("OnEvent", function(_, evt, msg)
-	if evt == "PLAYER_LOGIN" then
-		ShowFriends() --force a friends list update on login
-		good[UnitName("player")] = true --add ourself to safe list
-		if type(BADBOY_LEVEL) ~= "number" or BADBOY_LEVEL < 1 then
-			BADBOY_LEVEL = nil
+badboy:SetScript("OnEvent", function(frame, evt, msg)
+	ShowFriends() --force a friends list update on login
+	good[UnitName("player")] = true --add ourself to safe list
+	if type(BADBOY_LEVELS) ~= "table" then
+		BADBOY_LEVELS = {level = 2, dklevel = 58, dhlevel = 101, blockall = false, allowfriends = false, allowguild = false, allowgroup = false}
+		-- XXX TEMPORARY
+		local msg = "BadBoy_Levels: Your settings have been reset and expanded."
+		local locale = GetLocale()
+		if locale ~= "enUS" then
+			msg = msg .. " Translations required."
 		end
-	elseif evt == "CHAT_MSG_SYSTEM" then
-		if msg == ERR_FRIEND_LIST_FULL then
-			print("|cFF33FF99BadBoy_Levels|r: ", err) --print a warning if we see a friends full message
-			return
-		end
-	else
-		if not login then --run on login only
-			login = true
-			local num = GetNumFriends()
-			for i = 1, num do
-				local n = GetFriendInfo(i)
-				--add friends to safe list
-				if n then good[n] = true end
-			end
-			return
-		end
+		message(msg)
+		-- XXX
+	end
+	frame:UnregisterEvent("PLAYER_LOGIN")
+	frame:RegisterEvent("FRIENDLIST_UPDATE")
+	frame:RegisterEvent("CHAT_MSG_SYSTEM")
 
-		local num = GetNumFriends() --get total friends
-		for i = 1, num do
-			local player, level = GetFriendInfo(i)
-			--sometimes a friend will return nil, I have no idea why, so force another update
-			if not player then
-				ShowFriends()
-			else
-				if maybe[player] then --do we need to process this person?
-					RemoveFriend(player, true) --Remove player from friends list, the 2nd arg "true" is a fake arg added by request of tekkub, author of FriendsWithBenefits
-					if type(level) ~= "number" then
-						print("|cFF33FF99BadBoy_Levels|r: Level wasn't a number, tell BadBoy author! It was:", level)
-						error("|cFF33FF99BadBoy_Levels|r: Level wasn't a number, tell BadBoy author! It was: ".. tostring(level))
-					end
-					if level < filterTable[player] then
-						--lower than level 2, or a level defined by the user = bad,
-						--or lower than 58 and class is a Death Knight,
-						--so whisper the bad player what level they must be to whisper us
-						SendChatMessage(whisp:format(filterTable[player]), "WHISPER", nil, player)
-						for _, v in pairs(maybe[player]) do
-							for _, p in pairs(v) do
-								wipe(p) --remove player data table
-							end
-							wipe(v) --remove player data table
+	frame:SetScript("OnEvent", function(frame, evt, msg)
+		if evt == "CHAT_MSG_SYSTEM" then
+			if msg == ERR_FRIEND_LIST_FULL then
+				print("|cFF33FF99BadBoy_Levels|r: ", err) --print a warning if we see a friends full message
+				return
+			end
+		else
+			if not login then --run on login only
+				login = true
+				local num = GetNumFriends()
+				for i = 1, num do
+					local n = GetFriendInfo(i)
+					--add friends to safe list
+					if n then good[n] = true end
+				end
+				return
+			end
+
+			local num = GetNumFriends() --get total friends
+			for i = 1, num do
+				local player, level = GetFriendInfo(i)
+				--sometimes a friend will return nil, I have no idea why, so force another update
+				if not player then
+					ShowFriends()
+				else
+					if maybe[player] then --do we need to process this person?
+						RemoveFriend(player, true) --Remove player from friends list, the 2nd arg "true" is a fake arg added by request of tekkub, author of FriendsWithBenefits
+						if type(level) ~= "number" then
+							print("|cFF33FF99BadBoy_Levels|r: Level wasn't a number, tell BadBoy author! It was:", level)
+							error("|cFF33FF99BadBoy_Levels|r: Level wasn't a number, tell BadBoy author! It was: ".. tostring(level))
 						end
-					else
-						good[player] = true --higher = good
-						--get all the frames, incase whispers are being recieved in more that one chat frame
-						for _, v in pairs(maybe[player]) do
-							--get all the chat lines (queued if multiple) for restoration back to the chat frame
-							for _, p in pairs(v) do
-								--this player is good, we must restore the whisper(s) back to chat
-								if IsAddOnLoaded("WIM") then --WIM compat
-									WIM.modules.WhisperEngine:CHAT_MSG_WHISPER(select(3, unpack(p)))
-								elseif IsAddOnLoaded("Cellular") then --Cellular compat
-									local _,_,a1,a2,_,_,_,a6,_,_,_,_,a11,a12 = unpack(p)
-									Cellular:IncomingMessage(a2, a1, a6, nil, a11, a12)
-								else
-									ChatFrame_MessageEventHandler(unpack(p))
+						if level < filterTable[player] then
+							--Whisper the bad player what level they must be to whisper us
+							SendChatMessage(whisp:format(filterTable[player]), "WHISPER", nil, player)
+							for _, v in pairs(maybe[player]) do
+								for _, p in pairs(v) do
+									wipe(p) --remove player data table
 								end
-								wipe(p) --remove player data table
+								wipe(v) --remove player data table
 							end
-							wipe(v) --remove player data table
+						else
+							good[player] = true --higher = good
+							--get all the frames, incase whispers are being recieved in more that one chat frame
+							for _, v in pairs(maybe[player]) do
+								--get all the chat lines (queued if multiple) for restoration back to the chat frame
+								for _, p in pairs(v) do
+									--this player is good, we must restore the whisper(s) back to chat
+									if IsAddOnLoaded("WIM") then --WIM compat
+										WIM.modules.WhisperEngine:CHAT_MSG_WHISPER(select(3, unpack(p)))
+									elseif IsAddOnLoaded("Cellular") then --Cellular compat
+										local _,_,a1,a2,_,_,_,a6,_,_,_,_,a11,a12 = unpack(p)
+										Cellular:IncomingMessage(a2, a1, a6, nil, a11, a12)
+									else
+										ChatFrame_MessageEventHandler(unpack(p))
+									end
+									wipe(p) --remove player data table
+								end
+								wipe(v) --remove player data table
+							end
 						end
+						wipe(maybe[player]) --remove player data table
+						maybe[player] = nil --remove remaining empty table
 					end
-					wipe(maybe[player]) --remove player data table
-					maybe[player] = nil --remove remaining empty table
 				end
 			end
 		end
-	end
+	end)
 end)
 
 --incoming whisper filtering function
 ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", function(...)
 	local f, _, _, player, _, _, _, flag, _, _, _, _, id, guid = ...
 	local trimmedPlayer = Ambiguate(player, "none")
+
+	--[[ Start functionality for blocking all whispers regardless of level ]]--
+	if BADBOY_LEVELS.blockall and flag ~= "GM" and flag ~= "DEV" then
+		local allow = false
+
+		if BADBOY_LEVELS.allowfriends then
+			local _, characterName = BNGetGameAccountInfoByGUID(guid)
+			if characterName or IsCharacterFriend(guid) then
+				allow = true
+			end
+		end
+		if BADBOY_LEVELS.allowguild and IsGuildMember(guid) then
+			allow = true
+		end
+		if BADBOY_LEVELS.allowgroup and (UnitInRaid(trimmedPlayer) or UnitInParty(trimmedPlayer)) then
+			allow = true
+		end
+
+		if not allow then
+			SendChatMessage(whisp_notallowed, "WHISPER", nil, trimmedPlayer)
+			return true
+		else
+			return
+		end
+	end
+	--[[ End functionality for blocking all whispers regardless of level ]]--
+
 	--don't filter if good, GM, guild member, or x-server
 	if good[trimmedPlayer] or trimmedPlayer:find("%-") then return end
 	if BadBoyIsFriendly(trimmedPlayer, flag, id, guid) then return end
@@ -172,8 +218,12 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", function(...)
 	end
 	--Decide the level to be filtered
 	local _, englishClass = GetPlayerInfoByGUID(guid)
-	local level = BADBOY_LEVEL and tonumber(BADBOY_LEVEL)+1 or 2
-	if englishClass == "DEATHKNIGHT" and level < 58 then level = 58 end
+	local level = BADBOY_LEVELS.level
+	if englishClass == "DEATHKNIGHT" then
+		level = BADBOY_LEVELS.dklevel
+	elseif englishClass == "DEMONHUNTER" then
+		level = BADBOY_LEVELS.dhlevel
+	end
 	--Don't try to add a player to friends several times for 1 whisper (registered to more than 1 chat frame)
 	if not filterTable[trimmedPlayer] or filterTable[trimmedPlayer] ~= level then
 		filterTable[trimmedPlayer] = level
--- SimpleOptions.lua
+++ SimpleOptions.lua
@@ -1,56 +1,326 @@
 
-local levelsBox = CreateFrame("EditBox", nil, BadBoyConfig, "InputBoxTemplate")
-levelsBox:SetPoint("TOPLEFT", BadBoyLevelsConfigTitle, "BOTTOMLEFT", 5, 0)
-levelsBox:SetAutoFocus(false)
-levelsBox:SetNumeric(true)
-levelsBox:SetWidth(30)
-levelsBox:SetHeight(20)
-levelsBox:SetMaxLetters(3)
-levelsBox:Show()
-
-levelsBox:SetScript("OnHide", function(frame)
-	local n = tonumber(frame:GetText())
-	if not n or n == "" then frame:SetText(BADBOY_LEVEL or 1) print("|cFF33FF99BadBoy_Levels|r == "..(BADBOY_LEVEL or 1)) return end
-	if n < 1 then
-		frame:SetText(BADBOY_LEVEL or 1)
-		print("|cFF33FF99BadBoy_Levels|r == "..(BADBOY_LEVEL or 1))
-	else
-		BADBOY_LEVEL = n
-		print("|cFF33FF99BadBoy_Levels|r == "..n)
-	end
-end)
-levelsBox:SetScript("OnShow", function(frame)
-	frame:SetText(BADBOY_LEVEL or 1)
-end)
-
-local note = "Note: Death Knights lower than level 58 are blocked."
+local L_reqlevels = "Required Levels"
+local L_others = "Others"
+local L_deathknight = "Death Knight"
+local L_demonhunter = "Demon Hunter"
+local L_blockall = "Block All Whispers"
+local L_allowfriends = "Allow Friends"
+local L_allowguild = "Allow Guild"
+local L_allowgroup = "Allow Group"
 do
 	local L = GetLocale()
 	if L == "esES" or L == "esMX" then
-
+		--L_reqlevels = "Required Levels"
+		--L_others = "Others"
+		--L_deathknight = "Death Knight"
+		--L_demonhunter = "Demon Hunter"
+		--L_blockall = "Block All Whispers"
+		--L_allowfriends = "Allow Friends"
+		--L_allowguild = "Allow Guild"
+		--L_allowgroup = "Allow Group"
 	elseif L == "ptBR" then
-		note = "Nota: Cavaleiros da Morte com nível menor que 58 serão bloqueados."
+		--L_reqlevels = "Required Levels"
+		--L_others = "Others"
+		--L_deathknight = "Death Knight"
+		--L_demonhunter = "Demon Hunter"
+		--L_blockall = "Block All Whispers"
+		--L_allowfriends = "Allow Friends"
+		--L_allowguild = "Allow Guild"
+		--L_allowgroup = "Allow Group"
 	elseif L == "deDE" then
-		note = "Hinweis: Todesritter unter Stufe 58 werden blockiert."
+		--L_reqlevels = "Required Levels"
+		--L_others = "Others"
+		--L_deathknight = "Death Knight"
+		--L_demonhunter = "Demon Hunter"
+		--L_blockall = "Block All Whispers"
+		--L_allowfriends = "Allow Friends"
+		--L_allowguild = "Allow Guild"
+		--L_allowgroup = "Allow Group"
 	elseif L == "frFR" then
-
+		--L_reqlevels = "Required Levels"
+		--L_others = "Others"
+		--L_deathknight = "Death Knight"
+		--L_demonhunter = "Demon Hunter"
+		--L_blockall = "Block All Whispers"
+		--L_allowfriends = "Allow Friends"
+		--L_allowguild = "Allow Guild"
+		--L_allowgroup = "Allow Group"
 	elseif L == "ruRU" then
-		note = "Примечание: блокируются рыцари смерти, уровень которых ниже 58."
+		--L_reqlevels = "Required Levels"
+		--L_others = "Others"
+		--L_deathknight = "Death Knight"
+		--L_demonhunter = "Demon Hunter"
+		--L_blockall = "Block All Whispers"
+		--L_allowfriends = "Allow Friends"
+		--L_allowguild = "Allow Guild"
+		--L_allowgroup = "Allow Group"
 	elseif L == "koKR" then
-
+		--L_reqlevels = "Required Levels"
+		--L_others = "Others"
+		--L_deathknight = "Death Knight"
+		--L_demonhunter = "Demon Hunter"
+		--L_blockall = "Block All Whispers"
+		--L_allowfriends = "Allow Friends"
+		--L_allowguild = "Allow Guild"
+		--L_allowgroup = "Allow Group"
 	elseif L == "zhTW" then
-
+		--L_reqlevels = "Required Levels"
+		--L_others = "Others"
+		--L_deathknight = "Death Knight"
+		--L_demonhunter = "Demon Hunter"
+		--L_blockall = "Block All Whispers"
+		--L_allowfriends = "Allow Friends"
+		--L_allowguild = "Allow Guild"
+		--L_allowgroup = "Allow Group"
 	elseif L == "zhCN" then
-		note = "备注：低于58级的死亡骑士会被屏蔽。"
+		--L_reqlevels = "Required Levels"
+		--L_others = "Others"
+		--L_deathknight = "Death Knight"
+		--L_demonhunter = "Demon Hunter"
+		--L_blockall = "Block All Whispers"
+		--L_allowfriends = "Allow Friends"
+		--L_allowguild = "Allow Guild"
+		--L_allowgroup = "Allow Group"
 	elseif L == "itIT" then
-
+		--L_reqlevels = "Required Levels"
+		--L_others = "Others"
+		--L_deathknight = "Death Knight"
+		--L_demonhunter = "Demon Hunter"
+		--L_blockall = "Block All Whispers"
+		--L_allowfriends = "Allow Friends"
+		--L_allowguild = "Allow Guild"
+		--L_allowgroup = "Allow Group"
 	end
 end
 
-local dkText = levelsBox:CreateFontString(nil, nil, "GameFontHighlight")
-dkText:SetPoint("LEFT", levelsBox, "RIGHT", 10, 0)
-dkText:SetText(note)
-dkText:SetTextColor(0.5, 0.5, 0.5)
+--[[ DH input box ]]--
+local levelsBoxDH = CreateFrame("EditBox", nil, BadBoyConfig, "InputBoxTemplate")
+levelsBoxDH:SetPoint("TOPLEFT", BadBoyLevelsConfigTitle, "BOTTOMLEFT", 5, -3)
+levelsBoxDH:SetAutoFocus(false)
+levelsBoxDH:SetNumeric(true)
+levelsBoxDH:SetWidth(30)
+levelsBoxDH:SetHeight(20)
+levelsBoxDH:SetMaxLetters(3)
+levelsBoxDH:Show()
+
+levelsBoxDH:SetScript("OnEnter", function(self)
+	GameTooltip:SetOwner(self, "ANCHOR_TOP")
+	GameTooltip:AddLine(L_demonhunter, 0.5, 0.5, 0)
+	GameTooltip:Show()
+end)
+levelsBoxDH:SetScript("OnLeave", GameTooltip_Hide)
+levelsBoxDH:SetScript("OnHide", function(frame)
+	local n = tonumber(frame:GetText())
+	if not n or n < 1 then
+		return
+	else
+		BADBOY_LEVELS.dhlevel = n
+	end
+end)
+levelsBoxDH:SetScript("OnShow", function(frame)
+	frame:SetText(BADBOY_LEVELS.dhlevel)
+	if BADBOY_LEVELS.blockall then
+		frame:Disable()
+		frame:SetTextColor(0.5, 0.5, 0.5)
+	end
+end)
+
+--[[ DK input box ]]--
+local levelsBoxDK = CreateFrame("EditBox", nil, BadBoyConfig, "InputBoxTemplate")
+levelsBoxDK:SetPoint("LEFT", levelsBoxDH, "RIGHT", 10, 0)
+levelsBoxDK:SetAutoFocus(false)
+levelsBoxDK:SetNumeric(true)
+levelsBoxDK:SetWidth(30)
+levelsBoxDK:SetHeight(20)
+levelsBoxDK:SetMaxLetters(3)
+levelsBoxDK:Show()
+
+levelsBoxDK:SetScript("OnEnter", function(self)
+	GameTooltip:SetOwner(self, "ANCHOR_TOP")
+	GameTooltip:AddLine(L_deathknight, 0.5, 0.5, 0)
+	GameTooltip:Show()
+end)
+levelsBoxDK:SetScript("OnLeave", GameTooltip_Hide)
+levelsBoxDK:SetScript("OnHide", function(frame)
+	local n = tonumber(frame:GetText())
+	if not n or n < 1 then
+		return
+	else
+		BADBOY_LEVELS.dklevel = n
+	end
+end)
+levelsBoxDK:SetScript("OnShow", function(frame)
+	frame:SetText(BADBOY_LEVELS.dklevel)
+	if BADBOY_LEVELS.blockall then
+		frame:Disable()
+		frame:SetTextColor(0.5, 0.5, 0.5)
+	end
+end)
+
+--[[ Other input box ]]--
+local levelsBoxOther = CreateFrame("EditBox", nil, BadBoyConfig, "InputBoxTemplate")
+levelsBoxOther:SetPoint("LEFT", levelsBoxDK, "RIGHT", 10, 0)
+levelsBoxOther:SetAutoFocus(false)
+levelsBoxOther:SetNumeric(true)
+levelsBoxOther:SetWidth(30)
+levelsBoxOther:SetHeight(20)
+levelsBoxOther:SetMaxLetters(3)
+levelsBoxOther:Show()
+
+levelsBoxOther:SetScript("OnEnter", function(self)
+	GameTooltip:SetOwner(self, "ANCHOR_TOP")
+	GameTooltip:AddLine(L_others, 0.5, 0.5, 0)
+	GameTooltip:Show()
+end)
+levelsBoxOther:SetScript("OnLeave", GameTooltip_Hide)
+levelsBoxOther:SetScript("OnHide", function(frame)
+	local n = tonumber(frame:GetText())
+	if not n or n < 1 then
+		return
+	else
+		BADBOY_LEVELS.level = n
+	end
+end)
+levelsBoxOther:SetScript("OnShow", function(frame)
+	frame:SetText(BADBOY_LEVELS.level)
+	if BADBOY_LEVELS.blockall then
+		frame:Disable()
+		frame:SetTextColor(0.5, 0.5, 0.5)
+	end
+end)
+
+--[[ Input box text ]]--
+local reqLevelsTextBox = levelsBoxOther:CreateFontString(nil, nil, "GameFontHighlight")
+reqLevelsTextBox:SetPoint("LEFT", levelsBoxOther, "RIGHT", 10, 0)
+reqLevelsTextBox:SetText(L_reqlevels)
+
+--[[ Block all whispers checkbox 1/2 ]]--
+local blockAllBtn = CreateFrame("CheckButton", nil, BadBoyConfig, "OptionsBaseCheckButtonTemplate")
+blockAllBtn:SetPoint("TOPLEFT", levelsBoxDH, "BOTTOMLEFT", -5, 0)
+blockAllBtn:SetScript("OnShow", function(frame)
+	frame:SetChecked(BADBOY_LEVELS.blockall)
+	if BADBOY_LEVELS.blockall then
+		reqLevelsTextBox:SetTextColor(0.5, 0.5, 0.5)
+	end
+end)
+
+local blockAllBtnText = blockAllBtn:CreateFontString(nil, nil, "GameFontHighlight")
+blockAllBtnText:SetPoint("LEFT", blockAllBtn, "RIGHT", 0, 1)
+blockAllBtnText:SetText(L_blockall)
+
+--[[ Allow friends checkbox ]]--
+local allowFriendsBtn = CreateFrame("CheckButton", nil, BadBoyConfig, "OptionsBaseCheckButtonTemplate")
+allowFriendsBtn:SetPoint("TOPLEFT", blockAllBtn, "BOTTOMLEFT", 30, 5)
+allowFriendsBtn:SetScript("OnClick", function(frame)
+	local tick = frame:GetChecked()
+	BADBOY_LEVELS.allowfriends = tick
+	PlaySound(tick and "igMainMenuOptionCheckBoxOn" or "igMainMenuOptionCheckBoxOff")
+end)
+
+local allowFriendsBtnText = allowFriendsBtn:CreateFontString(nil, nil, "GameFontHighlight")
+allowFriendsBtnText:SetPoint("LEFT", allowFriendsBtn, "RIGHT", 0, 1)
+allowFriendsBtnText:SetText(L_allowfriends)
+
+allowFriendsBtn:SetScript("OnShow", function(frame)
+	frame:SetChecked(BADBOY_LEVELS.allowfriends)
+	if not BADBOY_LEVELS.blockall then
+		frame:Disable()
+		allowFriendsBtnText:SetTextColor(0.5, 0.5, 0.5)
+	end
+end)
+
+--[[ Allow guild checkbox ]]--
+local allowGuildBtn = CreateFrame("CheckButton", nil, BadBoyConfig, "OptionsBaseCheckButtonTemplate")
+allowGuildBtn:SetPoint("LEFT", allowFriendsBtn, "RIGHT", 110, 0)
+allowGuildBtn:SetScript("OnClick", function(frame)
+	local tick = frame:GetChecked()
+	BADBOY_LEVELS.allowguild = tick
+	PlaySound(tick and "igMainMenuOptionCheckBoxOn" or "igMainMenuOptionCheckBoxOff")
+end)
+
+local allowGuildBtnText = allowGuildBtn:CreateFontString(nil, nil, "GameFontHighlight")
+allowGuildBtnText:SetPoint("LEFT", allowGuildBtn, "RIGHT", 0, 1)
+allowGuildBtnText:SetText(L_allowguild)
+
+allowGuildBtn:SetScript("OnShow", function(frame)
+	frame:SetChecked(BADBOY_LEVELS.allowguild)
+	if not BADBOY_LEVELS.blockall then
+		frame:Disable()
+		allowGuildBtnText:SetTextColor(0.5, 0.5, 0.5)
+	end
+end)
+
+--[[ Allow group checkbox ]]--
+local allowGroupBtn = CreateFrame("CheckButton", nil, BadBoyConfig, "OptionsBaseCheckButtonTemplate")
+allowGroupBtn:SetPoint("LEFT", allowGuildBtn, "RIGHT", 110, 0)
+allowGroupBtn:SetScript("OnClick", function(frame)
+	local tick = frame:GetChecked()
+	BADBOY_LEVELS.allowgroup = tick
+	PlaySound(tick and "igMainMenuOptionCheckBoxOn" or "igMainMenuOptionCheckBoxOff")
+end)
+
+local allowGroupBtnText = allowGroupBtn:CreateFontString(nil, nil, "GameFontHighlight")
+allowGroupBtnText:SetPoint("LEFT", allowGroupBtn, "RIGHT", 0, 1)
+allowGroupBtnText:SetText(L_allowgroup)
+
+allowGroupBtn:SetScript("OnShow", function(frame)
+	frame:SetChecked(BADBOY_LEVELS.allowgroup)
+	if not BADBOY_LEVELS.blockall then
+		frame:Disable()
+		allowGroupBtnText:SetTextColor(0.5, 0.5, 0.5)
+	end
+end)
+
+--[[ Block all whispers checkbox 2/2 ]]--
+blockAllBtn:SetScript("OnClick", function(frame)
+	local tick = frame:GetChecked()
+	BADBOY_LEVELS.blockall = tick
+	if tick then
+		levelsBoxDH:Disable()
+		levelsBoxDH:SetTextColor(0.5, 0.5, 0.5)
+		levelsBoxDK:Disable()
+		levelsBoxDK:SetTextColor(0.5, 0.5, 0.5)
+		levelsBoxOther:Disable()
+		levelsBoxOther:SetTextColor(0.5, 0.5, 0.5)
+		reqLevelsTextBox:SetTextColor(0.5, 0.5, 0.5)
+
+		allowFriendsBtn:Enable()
+		allowFriendsBtnText:SetTextColor(1, 1, 1)
+
+		allowGuildBtn:Enable()
+		allowGuildBtnText:SetTextColor(1, 1, 1)
+
+		allowGroupBtn:Enable()
+		allowGroupBtnText:SetTextColor(1, 1, 1)
+
+		PlaySound("igMainMenuOptionCheckBoxOn")
+	else
+		levelsBoxDH:Enable()
+		levelsBoxDH:SetTextColor(1, 1, 1)
+		levelsBoxDK:Enable()
+		levelsBoxDK:SetTextColor(1, 1, 1)
+		levelsBoxOther:Enable()
+		levelsBoxOther:SetTextColor(1, 1, 1)
+		reqLevelsTextBox:SetTextColor(1, 1, 1)
+
+		BADBOY_LEVELS.allowfriends = false
+		allowFriendsBtn:Hide() allowFriendsBtn:Show()
+		allowFriendsBtn:Disable()
+		allowFriendsBtnText:SetTextColor(0.5, 0.5, 0.5)
+
+		BADBOY_LEVELS.allowguild = false
+		allowGuildBtn:Hide() allowGuildBtn:Show()
+		allowGuildBtn:Disable()
+		allowGuildBtnText:SetTextColor(0.5, 0.5, 0.5)
+
+		BADBOY_LEVELS.allowgroup = false
+		allowGroupBtn:Hide() allowGroupBtn:Show()
+		allowGroupBtn:Disable()
+		allowGroupBtnText:SetTextColor(0.5, 0.5, 0.5)
+
+		PlaySound("igMainMenuOptionCheckBoxOff")
+	end
+end)
 
 BadBoyLevelsConfigTitle:SetText("BadBoy_Levels @project-version@") -- Packager magic, replaced with tag version
 
